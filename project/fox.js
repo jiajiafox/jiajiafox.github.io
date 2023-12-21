3e5b2e0124222c3b843219999e58788926cb00a1
jiajiafox <oujiajia283@gmail.com>
2023-12-21 21:34:17 +0800

    Create fox.js

---
+++ project/fox.js
@@ -0,0 +1,28 @@
+alert("Hi 我是何嘉穎");
+document.writeln("javascript是我最愛的語言XD");
+console.log("我是這樣希望的");
+
+var count1="2.5"*"2.0";
+document.writeln(count1);
+
+var count2="2.5"+"2.0";
+document.writeln(count2);
+
+var conut3=parseFloat(count2)+2.0;
+document.writeln(conut3);
+
+var x = y =1;
+i=x++;
+j=++y;
+document.writeln(i+"",""+j);
+
+
+function car(a,b){
+    var x=0;
+    x=a+b;
+    return x;
+}
+var new_car=car(12,13);
+document.writeln(new_car);
+
+
