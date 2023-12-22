a9ffc7d7c54b1911a86f2740ef8a09056823dbbd
jiajiafox <oujiajia283@gmail.com>
2023-12-22 12:44:10 +0800

    Update fox2.js

--- project/fox2.js
+++ project/fox2.js
@@ -1,8 +1,12 @@
-function alary(x){
-    if(x>60000){
-        return "就任" ;
+
+function alary(y){
+    if(y>10){
+        return "結婚" ;
+    }else if (y<5){
+        return "我們不適合。。。" ; 
     }else{
-        return "不就任" ;
+        return "再加把勁!" ;
     }}
-var msg=alary(100000);
+var msg=alary(11);
 document.writeln(msg);
+
