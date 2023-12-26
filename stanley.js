ee515e016236eff815acb204afa84d1326c226b6
jiajiafox <oujiajia283@gmail.com>
2023-12-26 21:32:47 +0800

    Update stanley.js

--- stanley.js
+++ stanley.js
@@ -1,5 +1,7 @@
-var str = "never give up";
-alert(str + "!!!");
-document.writeln(typeof(str) + "+dw");
-console.log(str + "+log");
-
+function validate(){
+    var username=document.getElementById("username").value;
+    var email=document.getElementById("email").value;
+    var phone=document.getElementById("phone").value;
+    var password=document.getElementById("password").value;
+    var confirmpassword=document.getElementById("confirmpassword").value;
+}
