99344aa4d3034bf166f64016abd404a73772ed7d
jiajiafox <oujiajia283@gmail.com>
2023-12-28 18:36:09 +0800

    Delete stanley.js

--- stanley.js
+++
@@ -1,7 +0,0 @@
-function validate(){
-    var username=document.getElementById("username").value;
-    var email=document.getElementById("email").value;
-    var phone=document.getElementById("phone").value;
-    var password=document.getElementById("password").value;
-    var confirmpassword=document.getElementById("confirmpassword").value;
-}
