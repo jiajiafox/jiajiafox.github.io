9a425581c76a32c8d0320f9d1ef61223bb1cb46f
jiajiafox <oujiajia283@gmail.com>
2024-01-02 20:03:49 +0800

    Create stanley.php

---
+++ stanley/stanley.php
@@ -0,0 +1,12 @@
+<?php 
+print "正常的一行文字<br>";
+print         "前面有很多空白的一行文字<br>";
+print "後面有很多空白的一行文字<br>"             ;
+print "分號換行的一行文字<br>"
+  ;
+print"黏緊緊的一行文字<br>";
+print"       前面中間都加   很多空白的一行文字    <br>";
+?>
+<?php print "Hello"; print "PHP"; 
+?>
+<?php phpinfo();?>
