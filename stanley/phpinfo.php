528ef952e2825b41eda106bb557d997da95015d1
jiajiafox <oujiajia283@gmail.com>
2024-01-04 22:09:21 +0800

    Update phpinfo.php

--- stanley/phpinfo.php
+++ stanley/phpinfo.php
@@ -1,1 +1,25 @@
-<?php phpinfo(); ?>
+<!--<?php phpinfo(); ?>-->
+ <table border="1">
+        <tr>
+            <td>編號</td>
+            <td>諮詢地點</td>
+            <td>服務時段</td>
+            <td>聯絡電話</td>
+            <td>備註</td>
+        </tr>
+    </table>
+    <table border="1">
+        <?php
+        for($i=0;$i<5;$i++){
+            echo " <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>" 
+        }
+        ?>
+        
+    </table>
+   
