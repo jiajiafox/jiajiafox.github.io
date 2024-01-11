817849a314d85f836b0fc889237ad04ac984d3fa
jiajiafox <oujiajia283@gmail.com>
2024-01-11 21:34:24 +0800

    Create function.php

---
+++ php/function.php
@@ -0,0 +1,9 @@
+<?php
+function page_header($title){
+    echo  '<!Doctype html><html lang="zh-tw"><head><meta charset="UTF-8"><title>'.$title.'</title></head><body>
+      <div class="container"><header><table border="0"><tr><th><img src="green-island.jpg" width="135" height="150" align="center" boder="1"></th><th align="left"><h1>Stanley-何嘉穎</h1><strong>26/Male</strong></th></tr></table></header>';     
+}
+function page_footer(){
+ echo  "&copy;2024何嘉穎";
+}
+?>
