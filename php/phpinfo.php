3a34b639539521695ccc1bcc1055a7155c5f78fc
jiajiafox <oujiajia283@gmail.com>
2024-01-11 21:35:34 +0800

    Update phpinfo.php

--- php/phpinfo.php
+++ php/phpinfo.php
@@ -1,25 +1,10 @@
 <!--<?php phpinfo(); ?>-->
  <table border="1">
-        <tr>
-            <td>編號</td>
-            <td>諮詢地點</td>
-            <td>服務時段</td>
-            <td>聯絡電話</td>
-            <td>備註</td>
-        </tr>
-    </table>
-    <table border="1">
+        <tr><td>編號</td><td>諮詢地點</td><td>服務時段</td><td>聯絡電話</td><td>備註</td></tr>
         <?php
         for($i=0;$i<5;$i++){
-            echo " <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>" 
+            echo " <tr><td></td><td></td><td></td><td></td><td></td></tr>" 
         }
-        ?>
-        
-    </table>
+        ?>  
+</table>
    
