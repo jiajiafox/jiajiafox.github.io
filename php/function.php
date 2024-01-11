222cd7321a61b50f6f304e9ab553449f775e615e
jiajiafox <oujiajia283@gmail.com>
2024-01-11 21:35:10 +0800

    Update function.php

--- php/function.php
+++ php/function.php
@@ -1,9 +1,25 @@
 <?php
 function page_header($title){
-    echo  '<!Doctype html><html lang="zh-tw"><head><meta charset="UTF-8"><title>'.$title.'</title></head><body>
-      <div class="container"><header><table border="0"><tr><th><img src="green-island.jpg" width="135" height="150" align="center" boder="1"></th><th align="left"><h1>Stanley-何嘉穎</h1><strong>26/Male</strong></th></tr></table></header>';     
+echo <!Doctype html>
+<html lang="zh-tw">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Hello I am Stanley!</title>
+</head>
+        <header>
+            <table border="0">
+                <tr>
+                    <th><img src="image/green-island.jpg" width="135" height="150" align="center" boder="1"></th>
+                    <th align="left">
+                        <h1>Stanley-何嘉穎</h1>
+                        <strong>26/Male</strong>
+                    </th>
+                </tr>
+            </table>
+        </header>;
 }
 function page_footer(){
- echo  "&copy;2024何嘉穎";
+   echo "&copy;2024何嘉穎";
 }
 ?>
