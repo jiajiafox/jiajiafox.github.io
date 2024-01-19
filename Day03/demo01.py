2313f3440991e2ba72d2c20a2523827efa6ec40f
jiajiafox <oujiajia283@gmail.com>
2024-01-19 21:44:52 +0800

    Create demo01.py

---
+++ Day03/demo01.py
@@ -0,0 +1,13 @@
+# print("字串(數據)",end="\n")
+print("haha")#默認end='\n
+print("hihi")
+
+#end="轉義符號"
+# 轉義符號 : \n (換行) 、 \t (tab-空四格)
+print("haha",end="\t")
+print("hihi",end="\t")
+print("titi",end="?")
+print("",end="\n")
+
+print("I\'m Stanley")
+print("I\'m Stanley,I\'m a student!")
