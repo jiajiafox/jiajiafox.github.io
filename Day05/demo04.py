97dc660863bce1dda8e97bddba9780c188ed94dc
jiajiafox <oujiajia283@gmail.com>
2024-01-24 21:44:45 +0800

    Create demo04.py

---
+++ Day05/demo04.py
@@ -0,0 +1,16 @@
+#多重
+num=1
+a=1
+b=2
+if num==1:
+    print("第一段")
+elif num==2:
+    print("第二段")
+elif num==3:
+    print("第三段")
+elif num==4:
+    print("第四段")
+elif num==5:
+    print("第五段")
+else:
+    print("無")
