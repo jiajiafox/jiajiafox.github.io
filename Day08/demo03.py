58c04fa71a54dce0d61b2c26dc2760c095f058ee
jiajiafox <oujiajia283@gmail.com>
2024-01-31 21:43:15 +0800

    Create demo03.py

---
+++ Day08/demo03.py
@@ -0,0 +1,10 @@
+list1=[1000,20,300,40,50,60,70]
+list1.sort() #.sort()排序(按照大小)
+# print(list1)
+for i in list1:
+    print(i)
+
+
+# list2=[1000,20,300,40,50,60,70]
+# for j in list2:
+#     print(j)
