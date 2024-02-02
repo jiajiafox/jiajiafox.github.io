9b66aba9f7bca6c6572e04f6e34f76c1f3f4fd7b
jiajiafox <oujiajia283@gmail.com>
2024-02-02 21:47:38 +0800

    Create demo03.py

---
+++ demo03.py
@@ -0,0 +1,10 @@
+str1={10,20,30,40,50}
+str2={'a','b','c','d','e'}
+list=[1,2,3,4,5]
+tuple=(12.2,23.8,28,21,5)
+# str1.remove(100)
+# str1.discard(70)
+str1.pop()  #隨機刪除一個數據(不可控，少用)
+# list.pop()  
+print(str1)
+print(list)
