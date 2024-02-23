568dda195f839c693edc15eb334eb3c075db5ada
jiajiafox <oujiajia283@gmail.com>
2024-02-23 21:37:53 +0800

    Create demo04.py

---
+++ Day14/demo04.py
@@ -0,0 +1,19 @@
+# 模塊導入
+# 1:import 模塊名稱
+# 2:import 模塊名稱 as 別名
+# 3:import 模塊名稱1,模塊名稱2,模塊名稱3
+# 4:from 模塊名稱 import 功能名稱
+# 5:from 模塊名稱 import *
+# 6:from 模塊名稱 import 功能名稱 as 別名
+# 7:from 模塊名稱 import 功能名稱1,功能名稱2,功能名稱3
+
+
+# 1:import 模塊名稱
+# import math
+# print(math.sqrt(225))
+
+# 2:import 模塊名稱 as 別名
+# import math as ss
+# print(ss.sqrt(225))
+
+# 3:import 模塊名稱1,模塊名稱2,模塊名稱3
