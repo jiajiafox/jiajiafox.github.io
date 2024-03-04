a42a3178b998bae651798af961e975168f2e6cc1
jiajiafox <oujiajia283@gmail.com>
2024-03-04 21:30:24 +0800

    Create demo08.py

---
+++ Day17/demo08.py
@@ -0,0 +1,13 @@
+import mysql.connector
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",    # root最高權限，不會被擋。
+    passwd="Password123"
+)
+
+mycursor=mydb.cursor()
+mycursor.execute("use testDB")
+
+
+mycursor.execute("delete from students where name='E'")
+mydb.commit()
