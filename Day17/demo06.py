9fd00fefdd6d6a15c693f314251a25451664f477
jiajiafox <oujiajia283@gmail.com>
2024-03-04 20:53:31 +0800

    Create demo06.py

---
+++ Day17/demo06.py
@@ -0,0 +1,13 @@
+import mysql.connector
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",    # root最高權限，不會被擋。
+    passwd="Password123"
+)
+
+mycursor=mydb.cursor()
+mycursor.execute("use testDB")
+sqlsx="insert into students(name,age)values(%s,%s)"  # %s:字串
+student2=[("A",18),("B",20),("C",31),("D",45),("E",48)]
+mycursor.executemany(sqlsx,student2)    # 要新增多筆資料要改成many。
+mydb.commit()
