ff8baaf41a95042fec675c8ce84a3dd5efc8a8d1
jiajiafox <oujiajia283@gmail.com>
2024-03-06 20:49:36 +0800

    Update demo05.py

--- Day18/demo05.py
+++ Day18/demo05.py
@@ -1,5 +1,11 @@
 import numpy as np
-eye_array=np.eye()
+eye_array=np.eye(3)
 
 eye_array[1:]=3
+eye_array[:,0]=4
+# eye_array[:]=5
+eye_array[0,0]=10
 print(eye_array)
+
+sort_array=np.sort(eye_array)   # 排序(由小到大)
+print(sort_array)
