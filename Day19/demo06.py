483f0d7b95380811e207df3fa9848f36c1df1fba
jiajiafox <oujiajia283@gmail.com>
2024-03-08 21:39:10 +0800

    Update demo06.py

--- Day19/demo06.py
+++ Day19/demo06.py
@@ -3,3 +3,4 @@ import pandas as pd
 df=pd.read_csv(
 'https://data.moa.gov.tw/Service/OpenData/ForestRtBasic.aspx?FOTT=CSV&IsTransData=1&UnitId=D51')
 print(df.head)
+df.to_csv("xxxx.csv",encoding='UTF-8')
