e25447b9083b6509e54bd1bbba0097b0c6d038d5
jiajiafox <oujiajia283@gmail.com>
2024-03-13 20:48:33 +0800

    Update demo02.py

--- Day21/demo02.py
+++ Day21/demo02.py
@@ -5,4 +5,15 @@ plt.rc('font',family="Microsoft JhengHei")
 file=open('person.csv')
 read=csv.reader(file)
 header=next(file)
-print(header)
+data={"男":{"x":[],"y":[]},"女":{"x":[],"y":[]}}
+
+for row in read:
+    gender=row[0]
+    data[gender]["x"].append(int(row[1]))
+    data[gender]["y"].append(int(row[2]))
+    print(data)
+
+plt.scatter(data["男"]["x"],data["男"]["y"],label="男性")
+plt.scatter(data["女"]["x"],data["女"]["y"],label="女性")
+plt.legend()
+plt.show()
