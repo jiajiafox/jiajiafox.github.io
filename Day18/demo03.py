99bcfdb7b929311f2670496680a5e9de7a1e0beb
jiajiafox <oujiajia283@gmail.com>
2024-03-17 05:31:00 +0800

    Delete demo03.py

--- Day18/demo03.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-two_array=np.array([[1,3,5,7],
-                    [2,4,6,8],
-                    [10,11,12,13]])
-
-print(two_array[2][2])      # 12
-print(two_array[2,2])       # 12
-print(two_array[2])         # [10 11 12 13]
-print(two_array[2],[20])    # [10 11 12 13] [20]
