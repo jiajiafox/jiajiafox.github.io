b107e5c1009d88c306144bb4ae321601e5be5282
jiajiafox <oujiajia283@gmail.com>
2024-03-18 18:40:29 +0800

    Delete demo11.py

--- Day18/demo11.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-df=pd.DataFrame([('name','age','none'),
-                 ('Tom',18,'haha'),
-                 ('jason',25,pd.NA),
-                 ('mary',17,'hihi')])
-print(df.to_numpy())
-
