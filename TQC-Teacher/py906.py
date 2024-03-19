2b25ec71c4d17eacab5b7f5a3d6e1d7b8cb63006
jiajiafox <oujiajia283@gmail.com>
2024-03-19 13:12:07 +0800

    Create py906.py

---
+++ TQC-Teacher/py906.py
@@ -0,0 +1,12 @@
+fName=input()
+s1=input()
+s2=input()
+with open(fName,"r+",encoding="UTF-8") as file:
+    data=file.read()
+    print("=== Before the replacement")
+    print(data)
+    print("=== After the replacement")
+    data=data.replace(s1,s2)
+    print(data)
+    file.seek(0)
+    file.write(data)
