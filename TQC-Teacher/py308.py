795c7be7d646ccbe9b40ce3ba8861767a263f2e0
jiajiafox <oujiajia283@gmail.com>
2024-03-19 13:11:49 +0800

    Create py308.py

---
+++ TQC-Teacher/py308.py
@@ -0,0 +1,8 @@
+n=eval(input())
+for i in range(n):
+    tmp=num=eval(input())
+    sum=0
+    while(tmp>0):
+        sum+=tmp%10
+        tmp=tmp//10
+    print("Sum of all digits of %d is %d" %(num,sum))
