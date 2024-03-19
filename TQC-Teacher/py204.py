d471c54f37bd2a6b5ef47f84c7ef95a6941ae0a4
jiajiafox <oujiajia283@gmail.com>
2024-03-19 13:11:44 +0800

    Create py204.py

---
+++ TQC-Teacher/py204.py
@@ -0,0 +1,15 @@
+a = eval(input())
+b = eval(input())
+op = str(input())
+if op == "+":
+    print(a+b)
+elif op == "-":
+    print(a-b)
+elif op == "*":
+    print(a*b)
+elif op == "/":
+    print(a/b)
+elif op == "//":
+    print(a//b)
+elif op == "%":
+    print(a % b)
